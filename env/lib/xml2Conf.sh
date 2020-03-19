0680db0ac681cb75e60f3341942086b851a2f8ef
madhuwantha <academicformenew@gmail.com>
2020-03-19 23:02:52 +0530

    Machine-Learning-Course initial

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/hatadmin/Documents/ML/sample_project/env/lib"
+XML2_LIBS="-lxml2 -L/home/hatadmin/Documents/ML/sample_project/env/lib -lz -L/home/hatadmin/Documents/ML/sample_project/env/lib -llzma -lpthread -L/home/hatadmin/Documents/ML/sample_project/env/lib  -lm "
+XML2_INCLUDEDIR="-I/home/hatadmin/Documents/ML/sample_project/env/include/libxml2 -I/home/hatadmin/Documents/ML/sample_project/env/include"
+MODULE_VERSION="xml2-2.9.9"
+
